71526c696dfc215caa5110e1fdce7c266f6e6767
Nomddaay <92080450+Nomddaay@users.noreply.github.com>
2023-06-20 18:59:47 +0700

    add cnn

--- ImageClassification/KNN.py
+++ ImageClassification/KNN.py
@@ -1,8 +1,32 @@
-import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.datasets import load_digits
 
-x = [4, 5, 10, 4, 3, 11, 14 , 8, 10, 12]
-y = [21, 19, 24, 17, 16, 25, 24, 22, 21, 21]
-classes = [0, 0, 1, 0, 0, 1, 1, 0, 1, 1]
+# Chuẩn bị dữ liệu
+digits = load_digits()
+X = digits.data
+y = digits.target
 
-plt.scatter(x, y, c=classes)
-plt.show()
+# Chuẩn hoá dữ liệu
+scaler = StandardScaler()
+X_scaled = scaler.fit_transform(X)
+
+# Chia dữ liệu thành tập huấn luyện và tập kiểm tra
+X_train, X_test, y_train, y_test = train_test_split(X_scaled, y, test_size=0.2, random_state=42)
+
+# Xây dựng mô hình k-NN
+k = 3  # Số láng giềng gần nhất
+knn = KNeighborsClassifier(n_neighbors=k)
+
+# Huấn luyện mô hình
+knn.fit(X_train, y_train)
+
+# Dự đoán nhãn cho tập kiểm tra
+y_pred = knn.predict(X_test)
+
+# Đánh giá độ chính xác
+accuracy = accuracy_score(y_test, y_pred)
+print("Accuracy:", accuracy)
